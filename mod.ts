c724da9fba98918b80cd63e8f1bfc8ea3721542b
Ezeki Zibzibadze <ez@jok.io>
2023-01-01 23:30:12 +0400

    chore: update message

--- mod.ts
+++ mod.ts
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 
 function handler(_req: Request): Response {
-  return new Response("Hello, The World!!!");
+  return new Response("Hey, The World!!!");
 }
 
 console.log("Listening on http://localhost:8000");
